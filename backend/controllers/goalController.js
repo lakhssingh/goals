98124a2fc2890caafebfdc79f13bbde8d2da22ba
lakhssingh <lakhbir.shah@yahoo.co.uk>
2022-02-19 13:43:23 +0000

    Added authenication and authorization

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require('express-async-handler');
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 
 //@desc Get goals
 //@route GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-	const goals = await Goal.find();
+	const goals = await Goal.find({ user: req.user.id });
 	res.status(200).json(goals);
 });
 
@@ -20,6 +21,7 @@ const createGoal = asyncHandler(async (req, res) => {
 
 	const goal = await Goal.create({
 		text: req.body.text,
+		user: req.user.id,
 	});
 	res.status(200).json(goal);
 });
@@ -33,6 +35,20 @@ const updateGoal = asyncHandler(async (req, res) => {
 	if (!goal) {
 		res.status(400);
 		throw new Error('Goal not found');
+	}
+
+	const user = await User.findById(req.user.id);
+
+	//check for user
+	if (!user) {
+		res.status(401);
+		throw new Error('User not found');
+	}
+
+	//make sure the logged in user macthes the goal user
+	if (goal.user.toString() !== user.id) {
+		res.status(401);
+		throw new Error('User not authorised');
 	}
 
 	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -51,6 +67,18 @@ const deleteGoal = asyncHandler(async (req, res) => {
 	if (!goal) {
 		res.status(400);
 		throw new Error('Goal not found');
+	}
+
+	//check for user
+	if (!req.user) {
+		res.status(401);
+		throw new Error('User not found');
+	}
+
+	//make sure the logged in user macthes the goal user
+	if (goal.user.toString() !== req.user.id) {
+		res.status(401);
+		throw new Error('User not authorised');
 	}
 
 	await goal.remove();
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,8 +1,12 @@
-const { builtinModules } = require('module');
 const mongoose = require('mongoose');
 
 const goalsSchema = mongoose.Schema(
 	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
 		text: {
 			type: String,
 			required: [true, 'Please add a goal'],
